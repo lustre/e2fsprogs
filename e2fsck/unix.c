009701ee17473bb91182b8bd83131da0157b30d0
Andreas Dilger <andreas.dilger@intel.com>
2012-04-12 17:24:55 -0600

    e2fsck: parse config file before command-line opts
    The patch changes the order that the config file and command line
    are parsed so that command line has precedence.  It also parses
    the -E option for every occurrence, otherwise the -E option is
    not cumulative.
    
    Signed-off-by: Jim Garlick <garlick@llnl.gov>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>

--- e2fsck/unix.c
+++ e2fsck/unix.c
@@ -828,7 +828,6 @@ static errcode_t PRS(int argc, char *argv[], e2fsck_t *ret_ctx)
 #ifdef HAVE_SIGNAL_H
 	struct sigaction	sa;
 #endif
-	char		*extended_opts = 0;
 	char		*cp;
 	int 		res;		/* result of sscanf */
 #ifdef CONFIG_JBD_DEBUG
@@ -865,6 +864,12 @@ static errcode_t PRS(int argc, char *argv[], e2fsck_t *ret_ctx)
 		ctx->program_name = *argv;
 	else
 		usage(NULL);
+
+	cp = getenv("E2FSCK_CONFIG");
+	if (cp != NULL)
+		config_fn[0] = cp;
+	profile_set_syntax_err_cb(syntax_err_report);
+	profile_init(config_fn, &ctx->profile);
 
 	phys_mem_kb = get_memory_size() / 1024;
 	ctx->readahead_kb = ~0ULL;
@@ -903,7 +908,7 @@ static errcode_t PRS(int argc, char *argv[], e2fsck_t *ret_ctx)
 			ctx->options |= E2F_OPT_COMPRESS_DIRS;
 			break;
 		case 'E':
-			extended_opts = optarg;
+			parse_extended_opts(ctx, optarg);
 			break;
 		case 'p':
 		case 'a':
@@ -1070,8 +1075,6 @@ static errcode_t PRS(int argc, char *argv[], e2fsck_t *ret_ctx)
 			argv[optind]);
 		fatal_error(ctx, 0);
 	}
-	if (extended_opts)
-		parse_extended_opts(ctx, extended_opts);
 
 	/* Complain about mutually exclusive rebuilding activities */
 	if (getenv("E2FSCK_FIXES_ONLY"))
@@ -1088,11 +1091,6 @@ static errcode_t PRS(int argc, char *argv[], e2fsck_t *ret_ctx)
 			_("The -E bmap2extent and fixes_only options are incompatible."));
 		fatal_error(ctx, 0);
 	}
-
-	if ((cp = getenv("E2FSCK_CONFIG")) != NULL)
-		config_fn[0] = cp;
-	profile_set_syntax_err_cb(syntax_err_report);
-	profile_init(config_fn, &ctx->profile);
 
 	profile_get_boolean(ctx->profile, "options", "report_time", 0, 0,
 			    &c);
