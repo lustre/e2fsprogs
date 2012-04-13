b515ea4694967420b141947d0b53550cc85583cc
Andreas Dilger <andreas.dilger@intel.com>
2012-04-13 12:55:45 -0600

    debugfs: dump "fid" and "lma" xattrs on inode stat
    Print out the Lustre "fid" and "lma" object xattr contents,
    if present, with debugfs stat to simplify debugging.
    
      LU-8998 debugfs: handle combined LMA EA and PFID EA
    
      We need to store the stripe size and stripe count information in
      the OST object's PFID EA for the layout LFSCK to recover the MDT
      object or its (lost or corrupted) LOV EA. On the other hand, for
      PFL file, we also need to store its PFL ID and extent information
      in its OST object's PFID EA.
    
      Consider performance, we will store the PFID EA inside the inode
      body to avoid extra IO when read/write the PFID EA. Unfortunately,
      the space inside current 256-bytes inode for EA is very limited,
      and above enlarged PFID EA exceeds such limitation. So we have to
      make some hack with the PFID EA and LMA EA combined together. The
      patch makes the debugfs tool to handle such case properly.
    
      Signed-off-by: Fan Yong <fan.yong@intel.com>
      Change-Id: I1380d16d94d6d05272e22fdfff07ada038685669
      Reviewed-on: https://review.whamcloud.com/19436
      Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
      Tested-by: Jenkins
      Tested-by: Maloo <hpdd-maloo@intel.com>
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>

--- misc/fuse2fs.c
+++ misc/fuse2fs.c
@@ -2500,6 +2500,7 @@ out:
 
 static int count_buffer_space(char *name, char *value EXT2FS_ATTR((unused)),
 			      size_t value_len EXT2FS_ATTR((unused)),
+			      ext2_ino_t inode_num EXT2FS_ATTR((unused)),
 			      void *data)
 {
 	unsigned int *x = data;
@@ -2509,7 +2510,9 @@ static int count_buffer_space(char *name, char *value EXT2FS_ATTR((unused)),
 }
 
 static int copy_names(char *name, char *value EXT2FS_ATTR((unused)),
-		      size_t value_len EXT2FS_ATTR((unused)), void *data)
+		      size_t value_len EXT2FS_ATTR((unused)),
+		      ext2_ino_t inode_num EXT2FS_ATTR((unused)),
+		      void *data)
 {
 	char **b = data;
 
