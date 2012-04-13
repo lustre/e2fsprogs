e89568af33322fd84e0d920b51b6114e0fdfce35
Andreas Dilger <andreas.dilger@intel.com>
2012-04-13 12:55:45 -0600

    debugfs: decode and dump xattrs on inode stat
    Decode and print Access Control Lists in debugfs stat/ea_list.
    
    Print the Lustre "fid", "lma", and "link" xattr contents,
    if present, with "debugfs stat" to simplify debugging.
    Add "layout_version" and "range" values when printing a
    trusted.fid xattr.
    
    Increase the size of xattrs hex-dumped from 40 to 120.  But as
    Lustre encrypted inodes may contain illegal characters in their
    names, it is also needed to escape non-printable characters in
    linkEA.
    
    Change-Id: I3ee35ddb02582ab66d4e25ff76fae35935b54508
    Reviewed-on: https://review.whamcloud.com/43384
    Reviewed-by: Li Dongyang <dongyangli@ddn.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

--- misc/fuse2fs.c
+++ misc/fuse2fs.c
@@ -2541,6 +2541,7 @@ out:
 
 static int count_buffer_space(char *name, char *value EXT2FS_ATTR((unused)),
 			      size_t value_len EXT2FS_ATTR((unused)),
+			      ext2_ino_t inode_num EXT2FS_ATTR((unused)),
 			      void *data)
 {
 	unsigned int *x = data;
@@ -2550,7 +2551,9 @@ static int count_buffer_space(char *name, char *value EXT2FS_ATTR((unused)),
 }
 
 static int copy_names(char *name, char *value EXT2FS_ATTR((unused)),
-		      size_t value_len EXT2FS_ATTR((unused)), void *data)
+		      size_t value_len EXT2FS_ATTR((unused)),
+		      ext2_ino_t inode_num EXT2FS_ATTR((unused)),
+		      void *data)
 {
 	char **b = data;
 	size_t name_len = strlen(name);
