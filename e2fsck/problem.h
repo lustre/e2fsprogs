b5a78f57ba12fd19ee772c6269dbc455e3b233ce
Andreas Dilger <andreas.dilger@intel.com>
2012-04-12 18:03:37 -0600

    e2fsck: add support for expanding the inode size
    This patch adds a "-E expand_extra_isize" feature which makes sure
    that _every_ used inode has i_extra_isize >= s_min_extra_isize if
    s_min_extra_isize is set. Else it makes sure that i_extra_isize
    of every inode is equal to sizeof(ext2_inode_large) - 128.
    
    This is useful for the case where nanosecond timestamps or 64-bit
    inode version fields are required for all inodes in the filesystem.
    
    There is also a fix for test f_itable_collision, the original
    E2FSCK_TIME would overflow on a 32bit system when adding with
    ctx->time_fudge in EXT4_XTIME_FUTURE, making us increase the
    inode badness incorrectly.
    
      LU-10205 libext2fs: fix buffer overrun in ext2fs_expand_extra_isize
    
      In ext2fs_expand_extra_isize, we size buffer using 'size' but then
      do the memcpy with the rounded-up size, which can overflow the buffer.
    
      With MALLOC_CHECK_=2, I see:
      Error in `../e2fsck/e2fsck': free(): invalid pointer: <addr>
    
      Change-Id: I31be58de12d4d50646c7aa96959de0efc5c279c3
      Signed-off-by: Jeff Mahoney <jeffm@suse.com>
      Reviewed-on: https://review.whamcloud.com/29975
      Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
      Tested-by: Jenkins
      Tested-by: Maloo <hpdd-maloo@intel.com>
    
    Change-Id: I306ff4f81d8bd6bdf0446c76d6772951043fead4
    Signed-off-by: Kalpak Shah <kalpak@clusterfs.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>

--- e2fsck/problem.h
+++ e2fsck/problem.h
@@ -288,6 +288,15 @@ struct problem_context {
 /* Meta_bg and resize_inode are not compatible, remove resize_inode*/
 #define PR_0_DISABLE_RESIZE_INODE		0x000051
 
+/* Invalid s_min_extra_isize */
+#define PR_0_MIN_EXTRA_ISIZE_INVALID		0x000052
+
+/* Invalid s_want_extra_isize */
+#define PR_0_WANT_EXTRA_ISIZE_INVALID		0x000053
+
+/* Clear EXT4_FEATURE_RO_COMPAT_EXTRA_ISIZE flag */
+#define PR_0_CLEAR_EXTRA_ISIZE			0x000054
+
 /*
  * Pass 1 errors
  */
@@ -715,6 +724,25 @@ struct problem_context {
 
 /* Htree directory uses SipHash but should not */
 #define PR_1_HTREE_CANNOT_SIPHASH		0x01008E
+
+/* Warning for user that all inodes need to be expanded atleast by
+ * s_min_extra_isize
+ */
+#define PR_1_EXPAND_EISIZE_WARNING		0x01008F
+
+/* Expand the inode */
+#define PR_1_EXPAND_EISIZE			0x010090
+
+/* Delete an EA so that EXTRA_ISIZE may be enabled */
+#define PR_1_EISIZE_DELETE_EA			0x010091
+
+/* An EA needs to be deleted by e2fsck is being run with -p or -y */
+#define PR_1_EA_BLK_NOSPC			0x010092
+
+/* Disable EXTRA_ISIZE feature as inode cannot be expanded
+ * without deletion of an EA
+ */
+#define PR_1_CLEAR_EXTRA_ISIZE			0x010093
 
 /* Failed to goto block group */
 #define PR_1_SCAN_GOTO				0x0100A0
@@ -1282,6 +1310,9 @@ struct problem_context {
 
 /* Block bitmap checksum does not match */
 #define PR_5_BLOCK_BITMAP_CSUM_INVALID	0x05001B
+
+/* Expand the inodes which need a new EA block */
+#define PR_5_EXPAND_EISIZE		0x05001C
 
 /*
  * Post-Pass 5 errors
