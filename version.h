5730e722ef557ce0e83128cba349317f67e8260d
Darrick J. Wong <darrick.wong@oracle.com>
2015-10-24 00:24:57 -0400

    LU-8976 libext2fs: fix maximum bg overhead calculation with meta_bg enabled
    When meta_bg is enabled at mkfs time, we put at most one group
    descriptor block in each blockgroup.  Unfortunately, the calculation
    of max overhead per bg doesn't know this, so mkfs fails when it isn't
    strictly necessary.  Fix it, since Dave reported that he couldn't
    create a 500TB ext4 filesystem. 
    
    E2fsprogs-commit: 1abdd04eb1d19488493022f81c03f5b80aa7fcc7
    Change-Id: I22ef75037807f811b2133f4f4a62fbad02f1444f
    Reported-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-on: https://review.whamcloud.com/25263
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42.13.wc5"
-#define E2FSPROGS_DATE "15-Apr-2016"
+#define E2FSPROGS_VERSION "1.42.13.wc6"
+#define E2FSPROGS_DATE "05-Feb-2017"
