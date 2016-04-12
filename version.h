6448ad0247a6ebf272b028cacd5d313cdb6822d1
Andreas Dilger <andreas.dilger@intel.com>
2016-04-12 15:12:52 -0600

    LU-7867 e2fsprogs: update build version to 1.42.13.wc5
    Update build version to 1.42.13.wc5.
    
    Change-Id: I71b437b1cf0d4734a076b435f54bc5e263300c1e
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-on: http://review.whamcloud.com/19493
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Jian Yu <jian.yu@intel.com>
    Reviewed-by: Li Xi <lixi@ddn.com>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42.13.wc4"
-#define E2FSPROGS_DATE "28-Nov-2015"
+#define E2FSPROGS_VERSION "1.42.13.wc5"
+#define E2FSPROGS_DATE "15-Apr-2016"
