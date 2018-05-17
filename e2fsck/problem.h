77d77284b56287646e22d926a1e98a2d5b0176f3
Andreas Dilger <andreas.dilger@intel.com>
2018-05-17 13:43:16 +1000

    e2fsck: add support for dirdata feature
    Add support for the INCOMPAT_DIRDATA feature, which allows
    storing extra data in the directory entry beyond the name.
    This allows the Lustre File IDentifier to be accessed in
    an efficient manner, and would be useful for expanding a
    filesystem to allow more than 2^32 inodes in the future.
    
    Include this patches:
    
    e2fsck: e2fsck -D does not change dirdata content
    
    Fix dir optimization to preserve dirdata content for dot
    and dotdot entries.
    
    Lustre-bug: https://jira.hpdd.intel.com/browse/LU-1774
    Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
    Change-Id: Iae190794da75a2080a8e5cc5b95a49e0c894f72f
    
    e2fsprogs: Consider DIRENT_LUFID flag in link_proc().
    
    While adding the new file entry in directory block, link_proc()
    calculates minimum record length of the existing directory entry
    without considering the dirent data size and which leads to
    corruption. Changed the code to use EXT2_DIR_REC_LEN() which will
    return correct record length including dirent data size.
    
    Lustre-bug: https://jira.hpdd.intel.com/browse/LU-2462
    Signed-off-by: Manisha Salve <msalve@ddn.com>
    Change-Id: Ic593c558c47a78183143ec8e99d8385ac94d06f7
    
    libext2fs, e2fsck: don't use ext2_dir_entry_2
    
    Due to endian issues, do not use ext2_dir_entry_2 because it will
    have the wrong byte order on directory entries that are swabbed.
    Instead, use the standard practice of mask-and-shift to access the
    file_type and dirdata flags.
    
    Lustre-bug: https://jira.hpdd.intel.com/browse/LU-4677
    Signed-off-by: Pravin Shelar <pravin@clusterfs.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Artem Blagodarenko <artem.blagodarenko@gmail.com>

--- e2fsck/problem.h
+++ e2fsck/problem.h
@@ -1093,6 +1093,9 @@ struct problem_context {
 /* Inode is badly corrupt (badness value = ) */
 #define PR_2_INODE_TOOBAD		0x020055
 
+/* Entry dirdata length set incorrectly */
+#define PR_2_CLEAR_DIRDATA		0x020056
+
 /*
  * Pass 3 errors
  */
