39b4fd81575465e80b8e72f0c3ca3be95aae7bb6
Wang Shilong <wshilong@ddn.com>
2020-03-06 16:46:47 +0800

    e2fsck: serialize fix operations
    Allow different threads to fix at the same time could
    be dangerous and error-prone now, and most of time
    parallel scanning and checking is important.
    
    So this patch adds a mutex to serialize
    fix operations during pass1.
    
    And the good benefit of this, we don't need block
    allocations and free, superblock updates protection
    any more, since only fix operations during pass1
    could touch them.
    
    E2fsprogs-commit: 5485120b668c952b6e83f24a1e1a74eaa7bf362e
    
    Change-Id: Iedb64545240cc9325dc0c4ce737a970b27f4f3f6
    Signed-off-by: Wang Shilong <wshilong@ddn.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Signed-off-by: Saranya Muruganandam <saranyamohan@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- e2fsck/e2fsck.h
+++ e2fsck/e2fsck.h
@@ -480,6 +480,10 @@ struct e2fsck_struct {
 
 	/* Fast commit replay state */
 	struct e2fsck_fc_replay_state fc_replay_state;
+#ifdef HAVE_PTHREAD
+	/* serialize fix operation for multiple threads */
+	pthread_mutex_t		 fs_fix_mutex;
+#endif
 };
 
 #ifdef HAVE_PTHREAD
@@ -780,6 +784,8 @@ extern errcode_t e2fsck_allocate_subcluster_bitmap(ext2_filsys fs,
 						   const char *profile_name,
 						   ext2fs_block_bitmap *ret);
 unsigned long long get_memory_size(void);
+extern void e2fsck_pass1_fix_lock(e2fsck_t ctx);
+extern void e2fsck_pass1_fix_unlock(e2fsck_t ctx);
 
 /* unix.c */
 extern void e2fsck_clear_progbar(e2fsck_t ctx);
