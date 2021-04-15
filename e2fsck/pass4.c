lustre/e2fsprogs

commit 6528bce00beaa69d1d140452b1a8b84cc7e1f253

LU-11446 e2fsck: check trusted.link when fixing nlink
The inode link count could be higher than what is stored in the
local MDT inode because of remote file links from DNE MDTs.
If we find a mismatched link count, look up the "trusted.link"
xattr.  If it exists, do a sanity check on it, and use the
leh_reccount stored there if larger than the local link count.

If leh_overflow_time is set, then the "trusted.link" xattr may
not hold all of the links, so assume the maximum of available
link counts is valid until LFSCK clears leh_overflow_time.

Signed-off-by: Li Dongyang <dongyangli@ddn.com>
Change-Id: I213d816a92043c348eb55374aaa98e98957ccf23
Reviewed-on: https://review.whamcloud.com/43169
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Artem Blagodarenko <artem.blagodarenko@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

e2fsck/pass4.c
@@ -17,6 +17,7 @@
 #include "config.h"
 #include "e2fsck.h"
 #include "problem.h"
+#include "ext2fs/lfsck.h"
 #include <ext2fs/ext2_ext_attr.h>
 
 /*
@@ -26,7 +27,7 @@
  * This subroutine returns 1 then the caller shouldn't bother with the
  * rest of the pass 4 tests.
  */
-static int disconnect_inode(e2fsck_t ctx, ext2_ino_t i,
+static int disconnect_inode(e2fsck_t ctx, ext2_ino_t i, ext2_ino_t *last_ino,
 			    struct ext2_inode_large *inode)
 {
 	ext2_filsys fs = ctx->fs;
@@ -34,9 +35,12 @@ static int disconnect_inode(e2fsck_t ctx, ext2_ino_t i,
 	__u32 eamagic = 0;
 	int extra_size = 0;
 
-	e2fsck_read_inode_full(ctx, i, EXT2_INODE(inode),
-			       EXT2_INODE_SIZE(fs->super),
-			       "pass4: disconnect_inode");
+	if (*last_ino != i) {
+		e2fsck_read_inode_full(ctx, i, EXT2_INODE(inode),
+				       EXT2_INODE_SIZE(fs->super),
+				       "pass4: disconnect_inode");
+		*last_ino = i;
+	}
 	if (EXT2_INODE_SIZE(fs->super) > EXT2_GOOD_OLD_INODE_SIZE)
 		extra_size = inode->i_extra_isize;
 
@@ -75,6 +79,7 @@ static int disconnect_inode(e2fsck_t ctx, ext2_ino_t i,
 	if (fix_problem(ctx, PR_4_UNATTACHED_INODE, &pctx)) {
 		if (e2fsck_reconnect_file(ctx, i))
 			ext2fs_unmark_valid(fs);
+		*last_ino = 0;
 	} else {
 		/*
 		 * If we don't attach the inode, then skip the
@@ -87,20 +92,23 @@ static int disconnect_inode(e2fsck_t ctx, ext2_ino_t i,
 	return 0;
 }
 
-static void check_ea_inode(e2fsck_t ctx, ext2_ino_t i,
+/*
+ * This function is called when link_counted is zero. So this may not
+ * be an xattr inode at all. Return immediately if EA_INODE flag is not
+ * set.
+ */
+static void check_ea_inode(e2fsck_t ctx, ext2_ino_t i, ext2_ino_t *last_ino,
 			   struct ext2_inode_large *inode, __u16 *link_counted)
 {
 	__u64 actual_refs = 0;
 	__u64 ref_count;
 
-	/*
-	 * This function is called when link_counted is zero. So this may not
-	 * be an xattr inode at all. Return immediately if EA_INODE flag is not
-	 * set.
-	 */
-	e2fsck_read_inode_full(ctx, i, EXT2_INODE(inode),
-			       EXT2_INODE_SIZE(ctx->fs->super),
-			       "pass4: check_ea_inode");
+	if (*last_ino != i) {
+		e2fsck_read_inode_full(ctx, i, EXT2_INODE(inode),
+				       EXT2_INODE_SIZE(ctx->fs->super),
+				       "pass4: check_ea_inode");
+		*last_ino = i;
+	}
 	if (!(inode->i_flags & EXT4_EA_INODE_FL))
 		return;
 
@@ -132,6 +140,73 @@ static void check_ea_inode(e2fsck_t ctx, ext2_ino_t i,
 			e2fsck_write_inode(ctx, i, EXT2_INODE(inode), "pass4");
 		}
 	}
+}
+
+static errcode_t check_link_ea(e2fsck_t ctx, ext2_ino_t ino,
+			       ext2_ino_t *last_ino,
+			       struct ext2_inode_large *inode,
+			       __u16 *link_counted)
+{
+	struct ext2_xattr_handle *handle;
+	struct link_ea_header *leh;
+	void *buf;
+	size_t ea_len;
+	errcode_t retval;
+
+	if (*last_ino != ino) {
+		e2fsck_read_inode_full(ctx, ino, EXT2_INODE(inode),
+				       EXT2_INODE_SIZE(ctx->fs->super),
+				       "pass4: get link ea count");
+		*last_ino = ino;
+	}
+
+	retval = ext2fs_xattrs_open(ctx->fs, ino, &handle);
+	if (retval)
+		return retval;
+
+	retval = ext2fs_xattrs_read_inode(handle, inode);
+	if (retval)
+		goto err;
+
+	retval = ext2fs_xattr_get(handle, EXT2_ATTR_INDEX_TRUSTED_PREFIX
+				  LUSTRE_XATTR_MDT_LINK, &buf, &ea_len);
+	if (retval)
+		goto err;
+
+	leh = (struct link_ea_header *)buf;
+	if (leh->leh_magic == ext2fs_swab32(LINK_EA_MAGIC)) {
+		leh->leh_magic = LINK_EA_MAGIC;
+		leh->leh_reccount = ext2fs_swab32(leh->leh_reccount);
+		leh->leh_len = ext2fs_swab64(leh->leh_len);
+	}
+	if (leh->leh_magic != LINK_EA_MAGIC) {
+		retval = EINVAL;
+		goto err_free;
+	}
+	if (leh->leh_reccount == 0 && !leh->leh_overflow_time) {
+		retval = ENODATA;
+		goto err_free;
+	}
+	if (leh->leh_len > ea_len) {
+		retval = EINVAL;
+		goto err_free;
+	}
+
+	/* if linkEA overflowed and does not hold all links, assume *some*
+	 * links exist until LFSCK is next run and resets leh_overflow_time */
+	if (leh->leh_overflow_time) {
+		if (inode->i_links_count > *link_counted)
+			*link_counted = inode->i_links_count;
+		else if (*link_counted == 0)
+			*link_counted = 1111;
+	}
+	if (leh->leh_reccount > *link_counted)
+		*link_counted = leh->leh_reccount;
+err_free:
+	ext2fs_free_mem(&buf);
+err:
+	ext2fs_xattrs_close(&handle);
+	return retval;
 }
 
 void e2fsck_pass4(e2fsck_t ctx)
@@ -180,7 +255,8 @@ void e2fsck_pass4(e2fsck_t ctx)
 	inode = e2fsck_allocate_memory(ctx, inode_size, "scratch inode");
 
 	/* Protect loop from wrap-around if s_inodes_count maxed */
-	for (i=1; i <= fs->super->s_inodes_count && i > 0; i++) {
+	for (i = 1; i <= fs->super->s_inodes_count && i > 0; i++) {
+		ext2_ino_t last_ino = 0;
 		int isdir;
 
 		if (ctx->flags & E2F_FLAG_SIGNAL_MASK)
@@ -210,7 +286,8 @@ void e2fsck_pass4(e2fsck_t ctx)
 			 * check_ea_inode() will update link_counted if
 			 * necessary.
 			 */
-			check_ea_inode(ctx, i, inode, &link_counted);
+			check_ea_inode(ctx, i, &last_ino, inode, &link_counted);
+			check_link_ea(ctx, i, &last_ino, inode, &link_counted);
 		}
 
 		if (link_counted == 0) {
@@ -219,7 +296,7 @@ void e2fsck_pass4(e2fsck_t ctx)
 				     fs->blocksize, "bad_inode buffer");
 			if (e2fsck_process_bad_inode(ctx, 0, i, buf))
 				continue;
-			if (disconnect_inode(ctx, i, inode))
+			if (disconnect_inode(ctx, i, &last_ino, inode))
 				continue;
 			ext2fs_icount_fetch(ctx->inode_link_info, i,
 					    &link_count);
@@ -236,11 +313,18 @@ void e2fsck_pass4(e2fsck_t ctx)
 			}
 			link_counted = 1;
 		}
+		if (link_counted != link_count)
+			check_link_ea(ctx, i, &last_ino, inode, &link_counted);
+
 		if (link_counted != link_count) {
 			int fix_nlink = 0;
 
-			e2fsck_read_inode_full(ctx, i, EXT2_INODE(inode),
-					       inode_size, "pass4");
+			if (last_ino != i) {
+				e2fsck_read_inode_full(ctx, i,
+						       EXT2_INODE(inode),
+						       inode_size, "pass4");
+				last_ino = i;
+			}
 			pctx.ino = i;
 			pctx.inode = EXT2_INODE(inode);
 			if ((link_count != inode->i_links_count) && !isdir &&

lib/ext2fs/ext2fs.h
@@ -1291,6 +1291,8 @@ extern errcode_t ext2fs_adjust_ea_refcount3(ext2_filsys fs, blk64_t blk,
 					   ext2_ino_t inum);
 errcode_t ext2fs_xattrs_write(struct ext2_xattr_handle *handle);
 errcode_t ext2fs_xattrs_read(struct ext2_xattr_handle *handle);
+errcode_t ext2fs_xattrs_read_inode(struct ext2_xattr_handle *handle,
+				   struct ext2_inode_large *inode);
 errcode_t ext2fs_xattrs_iterate(struct ext2_xattr_handle *h,
 				int (*func)(char *name, char *value,
 					    size_t value_len,

lib/ext2fs/ext_attr.c
@@ -1024,9 +1024,11 @@ static void xattrs_free_keys(struct ext2_xattr_handle *h)
 	h->ibody_count = 0;
 }
 
-errcode_t ext2fs_xattrs_read(struct ext2_xattr_handle *handle)
+/* fetch xattrs from an already-loaded inode */
+errcode_t ext2fs_xattrs_read_inode(struct ext2_xattr_handle *handle,
+				   struct ext2_inode_large *inode)
 {
-	struct ext2_inode_large *inode;
+
 	struct ext2_ext_attr_header *header;
 	__u32 ea_inode_magic;
 	unsigned int storage_size;
@@ -1036,18 +1038,6 @@ errcode_t ext2fs_xattrs_read(struct ext2_xattr_handle *handle)
 	errcode_t err;
 
 	EXT2_CHECK_MAGIC(handle, EXT2_ET_MAGIC_EA_HANDLE);
-	i = EXT2_INODE_SIZE(handle->fs->super);
-	if (i < sizeof(*inode))
-		i = sizeof(*inode);
-	err = ext2fs_get_memzero(i, &inode);
-	if (err)
-		return err;
-
-	err = ext2fs_read_inode_full(handle->fs, handle->ino,
-				     (struct ext2_inode *)inode,
-				     EXT2_INODE_SIZE(handle->fs->super));
-	if (err)
-		goto out;
 
 	xattrs_free_keys(handle);
 
@@ -1083,7 +1073,7 @@ errcode_t ext2fs_xattrs_read(struct ext2_xattr_handle *handle)
 
 read_ea_block:
 	/* Look for EA in a separate EA block */
-	blk = ext2fs_file_acl_block(handle->fs, (struct ext2_inode *)inode);
+	blk = ext2fs_file_acl_block(handle->fs, EXT2_INODE(inode));
 	if (blk != 0) {
 		if ((blk < handle->fs->super->s_first_data_block) ||
 		    (blk >= ext2fs_blocks_count(handle->fs->super))) {
@@ -1114,20 +1104,39 @@ read_ea_block:
 		err = read_xattrs_from_buffer(handle, inode,
 					(struct ext2_ext_attr_entry *) start,
 					storage_size, block_buf);
-		if (err)
-			goto out3;
-
-		ext2fs_free_mem(&block_buf);
 	}
 
-	ext2fs_free_mem(&block_buf);
-	ext2fs_free_mem(&inode);
-	return 0;
-
 out3:
-	ext2fs_free_mem(&block_buf);
+	if (block_buf)
+		ext2fs_free_mem(&block_buf);
+out:
+	return err;
+}
+
+errcode_t ext2fs_xattrs_read(struct ext2_xattr_handle *handle)
+{
+	struct ext2_inode_large *inode;
+	size_t inode_size = EXT2_INODE_SIZE(handle->fs->super);
+	errcode_t err;
+
+	EXT2_CHECK_MAGIC(handle, EXT2_ET_MAGIC_EA_HANDLE);
+
+	if (inode_size < sizeof(*inode))
+		inode_size = sizeof(*inode);
+	err = ext2fs_get_memzero(inode_size, &inode);
+	if (err)
+		return err;
+
+	err = ext2fs_read_inode_full(handle->fs, handle->ino, EXT2_INODE(inode),
+				     EXT2_INODE_SIZE(handle->fs->super));
+	if (err)
+		goto out;
+
+	err = ext2fs_xattrs_read_inode(handle, inode);
+
 out:
 	ext2fs_free_mem(&inode);
+
 	return err;
 }
 

lib/ext2fs/lfsck.h
@@ -86,4 +86,26 @@ struct filter_fid {
 #define PFID_STRIPE_IDX_BITS	16
 #define PFID_STRIPE_COUNT_MASK	((1 << PFID_STRIPE_IDX_BITS) - 1)
 
+#ifndef LINK_EA_MAGIC
+/** Hardlink data is name and parent fid.
+ * Stored in this crazy struct for maximum packing and endian-neutrality */
+struct link_ea_entry {
+	/** lee_reclen is a __u16 stored big-endian, unaligned */
+	unsigned char	lee_reclen[2];	/* record size in bytes */
+	unsigned char	lee_parent_fid[sizeof(struct lu_fid)];
+	char		lee_name[0];	/* filename without trailing NUL */
+}__attribute__((packed));
+
+/** The link ea holds 1 \a link_ea_entry for each hardlink */
+#define LINK_EA_MAGIC 0x11EAF1DFUL
+struct link_ea_header {
+	__u32 leh_magic;		/* LINK_EA_MAGIC */
+	__u32 leh_reccount;		/* number of records in leh_entry[] */
+	__u64 leh_len;			/* total size in bytes */
+	__u32 leh_overflow_time;	/* when link xattr ran out of space */
+	__u32 padding;
+/*	struct link_ea_entry leh_entry; packed array of variable-size entries */
+};
+#endif
+
 #endif /* LFSCK_H */

tests/f_trusted_link/expect.1
@@ -1,0 +1,13 @@
+Pass 1: Checking inodes, blocks, and sizes
+Pass 2: Checking directory structure
+Pass 3: Checking directory connectivity
+Pass 4: Checking reference counts
+Inode 15 ref count is 7, should be 6.  Fix? yes
+
+Inode 16 ref count is 3, should be 6.  Fix? yes
+
+Pass 5: Checking group summary information
+
+test_filesys: ***** FILE SYSTEM WAS MODIFIED *****
+test_filesys: 17/4096 files (0.0% non-contiguous), 6424/16384 blocks
+Exit status is 1

tests/f_trusted_link/expect.2
@@ -1,0 +1,7 @@
+Pass 1: Checking inodes, blocks, and sizes
+Pass 2: Checking directory structure
+Pass 3: Checking directory connectivity
+Pass 4: Checking reference counts
+Pass 5: Checking group summary information
+test_filesys: 17/4096 files (0.0% non-contiguous), 6424/16384 blocks
+Exit status is 0

tests/f_trusted_link/name
@@ -1,0 +1,1 @@
+inode nlink according to trusted.link xattr

commit 87164b117be3bdeb1becf1960b80687637eda08f

LU-14600 e2fsck: check trusted.link after linking inode
If the inode is not linked into the namespace, link it into
lost+found before checking the trusted.link xattr to get the
DNE link count.

Fixes: 6528bce00bea ("LU-11446 e2fsck: check trusted.link when fixing nlink")
Test-Parameters: testlist=sanity-lfsck
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I447c1ce32f965311bfed1c08381928737dd9b02c
Reviewed-on: https://review.whamcloud.com/43324
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>

e2fsck/pass4.c
@@ -287,7 +287,6 @@ void e2fsck_pass4(e2fsck_t ctx)
 			 * necessary.
 			 */
 			check_ea_inode(ctx, i, &last_ino, inode, &link_counted);
-			check_link_ea(ctx, i, &last_ino, inode, &link_counted);
 		}
 
 		if (link_counted == 0) {
@@ -302,6 +301,7 @@ void e2fsck_pass4(e2fsck_t ctx)
 					    &link_count);
 			ext2fs_icount_fetch(ctx->inode_count, i,
 					    &link_counted);
+			check_link_ea(ctx, i, &last_ino, inode, &link_counted);
 		}
 		isdir = ext2fs_test_inode_bitmap2(ctx->inode_dir_map, i);
 		if (isdir && (link_counted > EXT2_LINK_MAX)) {