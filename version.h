b7742e844edf00cd8a20af2791d9d0ecb9e96a24
Andreas Dilger <adilger@whamcloud.com>
2021-12-16 22:09:12 -0700

    LU-15330 build: update version to 1.46.2.wc4
    92846e03 LU-15330 e2fsck: no parent lookup in disconnected dir
    53e89f07 LU-15164 misc: fix chattr usage message for project ID
    35ce354f LU-13717 sec: process Lustre enc inodes as normal enc inodes
    d35469d6 LU-13717 sec: support encrypted files handling in pfsck mode
    c6c08da9 LU-14953 e2fsck: pfsck progress report
    318931a7 LU-14784 contrib: add rocky linux rpm support
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: Ia81fd00adfffd35cd7a4b250aa1d974c8a80ed7f
    Reviewed-on: https://review.whamcloud.com/45875
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.46.2.wc3"
-#define E2FSPROGS_DATE "18-Jun-2021"
+#define E2FSPROGS_VERSION "1.46.2.wc4"
+#define E2FSPROGS_DATE "18-Dec-2021"
