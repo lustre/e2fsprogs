lustre/e2fsprogs

commit e5f0f3eebef7b483d5cc8b37023d6e0256776d94

Stop generating e2fsprogs.spec automatically
It adds extra complexity for not much gain, and stands in the way of
generating the release tarfile via git archive.

Signed-off-by: Theodore Ts'o <tytso@mit.edu>

e2fsprogs.spec
@@ -5,7 +5,7 @@
 
 Summary: Utilities for managing ext2/ext3/ext4 filesystems
 Name: e2fsprogs
-Version: @E2FSPROGS_PKGVER@
+Version: 1.44.3
 Release: 0
 License: GPLv2
 Group: System Environment/Base

.release-checklist
@@ -11,6 +11,7 @@
 	README
 	RELEASE-NOTES
 	e2fsprogs.lsm
+	e2fsprogs.spec
 	doc/libext2fs.texinfo (three places)
 
 6)  Make source distribution

Makefile.in
@@ -40,10 +40,7 @@ progs: all-progs-recursive
 libs: all-libs-recursive
 all-progs-recursive all-libs-recursive: subs
 
-e2fsprogs.spec: $(DEP_SUBSTITUTE) e2fsprogs.spec.in
-	cd $(top_builddir); CONFIG_FILES=./e2fsprogs.spec ./config.status
-
-rpm: e2fsprogs.spec
+rpm:
 	sh contrib/build-rpm
 
 docs:
@@ -117,7 +114,7 @@ clean:: clean-recursive clean-local clean-doc
 	$(RM) -f $(SUBS) 
 
 distclean: distclean-doc distclean-recursive
-	$(RM) -rf autom4te.cache e2fsprogs.spec ext2ed/Makefile po/stamp-po \
+	$(RM) -rf autom4te.cache ext2ed/Makefile po/stamp-po \
 		asm_types.h config.log public_config.h parse-types.log
 	$(MAKE) distclean-local 
 

configure
@@ -13877,7 +13877,7 @@ if test -z "$BLKID_CMT" ; then
 	blkid_out_list="lib/blkid/Makefile lib/blkid/blkid.pc \
 		lib/blkid/blkid_types.h"
 fi
-for i in MCONFIG Makefile e2fsprogs.spec \
+for i in MCONFIG Makefile \
 	util/Makefile util/subst.conf util/gen-tarball util/install-symlink \
 	lib/et/Makefile lib/ss/Makefile lib/e2p/Makefile \
 	lib/ext2fs/Makefile lib/ext2fs/ext2_types.h \

configure.ac
@@ -1486,7 +1486,7 @@ if test -z "$BLKID_CMT" ; then
 	blkid_out_list="lib/blkid/Makefile lib/blkid/blkid.pc \
 		lib/blkid/blkid_types.h"
 fi
-for i in MCONFIG Makefile e2fsprogs.spec \
+for i in MCONFIG Makefile \
 	util/Makefile util/subst.conf util/gen-tarball util/install-symlink \
 	lib/et/Makefile lib/ss/Makefile lib/e2p/Makefile \
 	lib/ext2fs/Makefile lib/ext2fs/ext2_types.h \

util/gen-tarball.in
@@ -40,7 +40,6 @@ if test -z "$SOURCE_DATE_EPOCH" ; then
     export SOURCE_DATE_EPOCH=$(cd $top_srcdir; git log -1 --pretty=%ct)
 fi
 
-mv ../e2fsprogs.spec $top_srcdir/e2fsprogs.spec
 (cd $top_srcdir/.. ; find $base_e2fsprogs \( -name \*~ -o -name \*.orig \
 		-o -name CVS -o -name \*.rej -o -name Makefile.pq \
 		-o -name TAGS -o -name \*.old -o -name SCCS \
@@ -56,5 +55,3 @@ sed -e "s;^;$base_e2fsprogs/;" < $srcdir/$list.exclude >> $exclude
     | $GZIP -9n -c > $tarout
 $GZIP -ln $tarout
 rm -f "$exclude"
-
-mv $top_srcdir/e2fsprogs.spec ../e2fsprogs.spec 

commit ba675c948e52c94aedef5ee4a5d96e06359afede

build: update version for Lustre build
Add Lustre-specific build version to distinguish packages from
upstream packages.

Since e2fsprogs release 1.44.4 the Version in e2fsprogs.spec
is not generated anymore. However contrib/build-rpm still relies
on the Version from the spec, maintain the version in the spec
as well.

Fixes: e5f0f3eebef7 ("Stop generating e2fsprogs.spec automatically")

Change-Id: I4a31e659ff4d98f9bb00fea731bd1ee59a55fd38
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>

e2fsprogs.spec
@@ -5,7 +5,7 @@
 
 Summary: Utilities for managing ext2/ext3/ext4 filesystems
 Name: e2fsprogs
-Version: 1.46.5
+Version: 1.46.5.wc1
 Release: 0
 License: GPLv2
 Group: System Environment/Base

version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.46.5"
-#define E2FSPROGS_DATE "30-Dec-2021"
+#define E2FSPROGS_VERSION "1.46.5.wc1"
+#define E2FSPROGS_DATE "30-Mar-2022"