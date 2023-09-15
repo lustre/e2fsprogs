be2d5186c739ce18601b6c673d02f2995cde261b
Andreas Dilger <adilger@whamcloud.com>
2023-09-15 02:08:09 -0600

    LU-17117 journal: increase revoke block hash size
    Increase the size of the revoke block hash table to scale with the
    size of the journal, so that we don't get long hash chains if there
    are a large number of revoke blocks in the journal to replay.
    
    The new hash size will default to 1/16 of the blocks in the journal.
    This is about 1 byte per block in the hash table, but there are two
    allocated.  The total amount of memory allocated for the revoke blocks
    depends much more on how many are in the journal, and not on the size
    of the hash table.  The system is regularly using this much memory for
    the journal blocks, so the hash table size is not a big factor.
    
    Consolidate duplicate code between recover_ext3_journal() and
    ext2fs_open_ext3_journal() in debugfs.c.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: Ibadf2a28c2f42fa92601f9da39a6ff73a43ebbe5
    Reviewed-on: https://review.whamcloud.com/c/tools/e2fsprogs/+/52386
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Reviewed-by: Li Dongyang <dongyangli@ddn.com>

--- lib/ext2fs/jfs_compat.h
+++ lib/ext2fs/jfs_compat.h
@@ -59,6 +59,7 @@ typedef __u64 u64;
 
 #define blkdev_issue_flush(kdev)	sync_blockdev(kdev)
 #define is_power_of_2(x)	((x) != 0 && (((x) & ((x) - 1)) == 0))
+#define roundup_power_of_two(n)		(1UL << (ext2fs_log2((n) - 1) + 1))
 #define pr_emerg(fmt)
 #define pr_err(...)
 
