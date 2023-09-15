b67310af4a9cf270def588d6f97229e8f6cce30e
Andreas Dilger <adilger@whamcloud.com>
2023-09-15 02:08:09 -0600

    LU-17117 journal: increase revoke block hash size
    Increase the size of the revoke block hash table to scale with the
    size of the journal, so that we don't get long hash chains if there
    are a large number of revoke blocks in the journal to replay.
    
    The new hash size will default to 1/16 of the blocks in the journal.
    This is about 1 byte per block in the hash table, but there are two
    allocated.  The total amount of memory allocated for the revoke blocks
    depends much more on how many are in the journal, and not on the size
    of the hash table.  The system is regularly using this much memory for
    the journal blocks, so the hash table size is not a big factor.
    
    Consolidate duplicate code between recover_ext3_journal() and
    ext2fs_open_ext3_journal() in debugfs.c.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: Ibadf2a28c2f42fa92601f9da39a6ff73a43ebbe5
    Reviewed-on: https://review.whamcloud.com/c/tools/e2fsprogs/+/52386
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Reviewed-by: Li Dongyang <dongyangli@ddn.com>

--- misc/util.c
+++ misc/util.c
@@ -50,6 +50,7 @@
 #include "ext2fs/ext2_fs.h"
 #include "ext2fs/ext2fs.h"
 #include "ext2fs/ext2fsP.h"
+#include "ext2fs/kernel-jbd.h"
 #include "support/nls-enable.h"
 #include "support/devname.h"
 #include "blkid/blkid.h"
@@ -295,12 +296,13 @@ void figure_journal_size(struct ext2fs_journal_params *jparams,
 		else if (!ext2fs_has_feature_fast_commit(fs->super))
 			jparams->num_fc_blocks = 0;
 		total_blocks = jparams->num_journal_blocks + jparams->num_fc_blocks;
-		if (total_blocks < min_size || total_blocks > 10240000) {
+		if (total_blocks < min_size ||
+		    total_blocks > JBD2_MAX_JOURNAL_BLOCKS) {
 			fprintf(stderr, _("\nThe total requested journal "
 				"size is %d blocks; it must be\n"
-				"between %d and 10240000 blocks.  "
-				"Aborting.\n"),
-				total_blocks, min_size);
+				"between %d and %lu blocks.  "
+				"Aborting.\n"), total_blocks, min_size,
+				JBD2_MAX_JOURNAL_BLOCKS);
 			exit(1);
 		}
 		if ((unsigned int) total_blocks > ext2fs_free_blocks_count(fs->super) / 2) {
